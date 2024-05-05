e999875464da9c7c8dde9f37657609983046f659
Icaro Silva Rodrigues <icaro2rodrigues@gmail.com>
2024-05-05 15:55:32 -0300

    feat: add test contrroller

--- src/main/java/com/gerenciador/tarefas/gerenciadortarefas/controller/TesteApiController.java
+++ src/main/java/com/gerenciador/tarefas/gerenciadortarefas/controller/TesteApiController.java
@@ -1,6 +1,7 @@
 package com.gerenciador.tarefas.gerenciadortarefas.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,8 +9,15 @@ public class TesteApiController {
 
 
     @GetMapping("/teste-api")
-    private String test () {
+    public String test () {
         return "Funfoooooo";
     }
+
+    @GetMapping("/teste-api-bem-vindo")
+
+    public String testeBemVindo(@RequestParam(name = "nome") String nome){
+        return "bem-vindo"+nome+"sinta-se a vontade";
+    }
+    
     
 }
